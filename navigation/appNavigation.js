1a0961260d5e02a27daeac138847c49551a7b220
Your Name <you@example.com>
2024-05-07 06:10:33 +0700

    added statusbar

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/navtive'
-export default function AppNavigation (){
+import { NavigationContainer } from '@react-navigation/navtive'
+import HomeScreen from '../screens/HomeScreen';
+export default function AppNavigation() {
 
-    return(
+    return (
 
-      <NavigationContainer>
-        <Stack.Navigation>
-
-        </Stack.Navigation>
-      </NavigationContainer>
+        <NavigationContainer>
+            <Stack.Navigation>
+                <Stack.Screen name="Home" component={HomeScreen} options={{ headersShown: false }} />
+            </Stack.Navigation>
+        </NavigationContainer>
     )
 
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Image, StatusBar } from 'react-native';
+import { SafeAreaView, TextInput, Touchable } from 'react-native-web';
+import { theme } from '../theme';
+
+export default function HomeScreen() {
+
+    return (
+
+        <View className="flex-1 relative ">
+            <StatusBar style="light" />
+            <Image source={require("../assets/images/bg.png")}
+                className="absolute h-full w-full" />
+            <SafeAreaView className="flex flex-1">
+                <View style={{ height: "7%" }} className="mx-4 relative z-50">
+                    <View className="flex-row justify-end items-center rounded-full"
+                        style={{ backgroundColor: theme.bgWhite(0.2) }}>
+                        <TextInput placeholder="Search" placeholderTextColor={"lightgray"}
+                            className="pl-6 h-10 flex-1 text-base text-white" />
+                            
+                    </View>
+                </View>
+            </SafeAreaView>
+        </View>
+    )
+
+}
