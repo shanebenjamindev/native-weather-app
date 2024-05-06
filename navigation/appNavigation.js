3bd6981d597db8e29594e65a54022ab8b64fc942
Your Name <you@example.com>
2024-05-07 03:12:00 +0700

    first comommit

---
+++ navigation/appNavigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/navtive'
+export default function AppNavigation (){
+
+    return(
+
+      <NavigationContainer>
+        <Stack.Navigation>
+
+        </Stack.Navigation>
+      </NavigationContainer>
+    )
+
+}
